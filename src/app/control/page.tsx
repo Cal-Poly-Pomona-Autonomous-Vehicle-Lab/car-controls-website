4ed3140271829098cd9c3b07fb7b0c506ce86fbf
Conspiciens <fernandezjohann08@gmail.com>
2024-11-25 02:10:10 -0800

    Now can handle when stream comes offline and sockets have timeouts + small fixes

--- src/app/control/page.tsx
+++ src/app/control/page.tsx
@@ -6,6 +6,7 @@ import { ChevronCompactDown, ChevronCompactLeft, ChevronCompactRight, ChevronBar
 import {io, Socket} from 'socket.io-client';
 import "./controlPage.css";
 import { useFormState } from 'react-dom';
+import { read } from "fs";
 
 export default function carControls() {
     let socket = useRef<Socket | null> (null); 
@@ -16,29 +17,34 @@ export default function carControls() {
     const [isRight, setRight] = useState(false);
     const [isForward, setForward] = useState(false);
     const [isBackward, setBackward] = useState(false);
+    const [isStream, setStream] = useState(false);
 
     socket.current = io("http://10.110.194.54:5002", {
+        timeout: 5000, 
         transports: ["websocket"], 
     });
 
     useEffect(() => {
-        socket.current!.on('connect', () => {
-            console.log("Conneted");
-            console.log(socket.current);
-        })
-        
+       isStreamLive(); 
+    }, [isStream]);
+
+    useEffect(() => {
         if (socket.current === null) {
             return; 
         }
 
+        socket.current.on('connect', () => {
+            console.log("Conneted");
+        })
+
         socket.current.on("disconnect", () => {
-            console.log(socket.current!.id); 
-          });
+            console.log("Disconected");
+        });
 
         socket.current.on("connect_error", (err) => {
             console.log(err.message);
         
-          })
+         })
     }, [])
 
     const sendKeyPressToRos = async(event: any) => {
@@ -65,11 +71,35 @@ export default function carControls() {
        }
     }
 
+    const isStreamLive = async() => {
+        try {
+            const req = await fetch("http://127.0.0.1:5000/");
+            if (!req.ok) {
+                return;
+            }
+
+            const reader = req.body?.getReader();
+
+            setStream(true); 
+            while (true){
+                const value = await reader?.read(); 
+                if (value?.done) { break }
+            }
+
+        } catch(error) {
+            console.log(error);
+        } finally {
+            setStream(false);
+        }
+
+    }
+
     return (
         <>
             <div className="allStreamControls" onKeyDown={sendKeyPressToRos} tabIndex={0}> 
                 <div className="videoStream" tabIndex={0}> 
-                    <img className="videoFrame" src="http://10.110.194.54:5002/camera/stream" /> 
+                    <div className="backgroundColor" /> 
+                    <img className="videoFrame" src={isStream ? "http://10.110.194.54:5002/camera/stream": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNgYAAAAAMAASsJTYQAAAAASUVORK5CYII="} /> 
                 </div>
                 <div className="controls" tabIndex={0}>
                     <ChevronCompactDown size={20}></ChevronCompactDown>
