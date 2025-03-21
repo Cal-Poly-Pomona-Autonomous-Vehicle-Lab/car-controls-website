432ce6b8ac85a324e8ce7c8d3bd035fb0f6176d6
Conspiciens <fernandezjohann08@gmail.com>
2025-03-21 10:27:12 -0700

    Publisher updated

--- MQTT/publisher.c
+++ MQTT/publisher.c
@@ -1,19 +1,27 @@
 #include <stdio.h> 
 #include <string.h>
+#include <unistd.h> 
+#include <stdint.h> 
+#include <getopt.h> 
+#include <sys/types.h> 
 
 #include "mosquitto.h"
 
-int main() {
-    struct mosquitto *mosq; 
-    ambient_t ambient; 
+typedef struct {
+    int is_alive = 1
+} heart_t; 
 
-    char mqtt_channel_name[256]; 
+int main() { 
+    struct mosquitto *mosq; 
+    heart_t heart; 
+
+    char mqtt_channel_name[256] = "init"; 
 
     start_arg_t start_arg = {
-        .broker_hostname = "localhost", 
+        .broker_hostname = "10.110.245.223", 
         .broker_port = 1883, 
-        .location = "location
-    } 
+        .location = "location",
+    };
     
     mosquitto_lib_init(); 
 
@@ -22,12 +30,16 @@ int main() {
     if (!mosq) 
         printf("Failed to create mosq\n"); 
 
-    if (moquitto_connect(mosq, start_arg.broker_name, start_arg.broker_port, 60) != MSQ_ERR_SUCESS) {
+    if (moquitto_connect(mosq, start_arg.broker_name, start_arg.broker_port, 60) != MSQ_ERR_SUCESS) 
         printf("Failed to connect to MQTT Broker\n"); 
 
+    
 
     while (1) {
-    } 
-    
-    
+        mosquitto.publish(mosq, NULL, mqtt_channel_name, sizeof(heart), &heart, MQTT_QOS_0, false);
+        sleep(1);
+    }
+
+    mosquitto_destroy(mosq);
+    mosquitto_lib_cleanup(); 
 } 
