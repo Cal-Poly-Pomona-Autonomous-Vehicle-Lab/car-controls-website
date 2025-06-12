46655bd7774bf58c35d7385aab5cf50055e3758e
Conspiciens <fernandezjohann08@gmail.com>
2025-06-12 08:35:01 -0700

    Reverted couple changes

--- heartbeat_cpp/main.c
+++ heartbeat_cpp/main.c
@@ -201,7 +201,6 @@ bool wait_for_update(int fd, fd_set *rfds, struct timeval *tv) {
 
 // Prioritize Ethernet over Wi-Fi
 int interface_priority(const char *name) {
-    
     if (strncmp(name, "eth", 3) == 0) return 1;
     else if (strncmp(name, "en", 2) == 0) return 2;  // macOS Ethernet
     else if (strncmp(name, "wlan", 4) == 0) return 3;
@@ -253,12 +252,10 @@ void get_interface(char *interface_address) {
         /* If this interface has higher priority (lower number), 
          save its IP as the best one found so far.
         */
-        if (prio > -1) { 
-            perror("Failed to get an ip address"); 
-            return; 
-        } 
-
-        strncpy(best_ip, ip, sizeof(best_ip));
+         if (prio < best_priority) {
+             best_priority = prio;
+             strncpy(best_ip, ip, sizeof(best_ip));
+        }
     } 
 
     freeifaddrs(ifaddr); 
