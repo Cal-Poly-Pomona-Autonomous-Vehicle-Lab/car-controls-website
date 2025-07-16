55bbf06923e6a334fccc41cf2e5e67099435a9e4
Conspiciens <fernandezjohann08@gmail.com>
2025-07-16 11:56:16 -0700

    Small updateS

--- cpp_api_control/src/main.cpp
+++ cpp_api_control/src/main.cpp
@@ -7,12 +7,12 @@ void init_opencv(cv::VideoCapture &cap, cv::Mat *frame) {
   while (cap.isOpened()) {
     cap >> *frame; 
 
-    if (frame == NULL || frame.empty()) {
+    if ( frame == NULL || frame->empty() ) {
       std::cout << "Error: unable to obtain frame! \n"; 
       return; 
     }
 
-    if (cv::waitKey(1) == 27) {
+    if ( cv::waitKey(1) == 27 ) {
       break; 
     }
   }
@@ -25,31 +25,35 @@ int main() {
   std::vector<int> param(2); 
 
   param[0] = cv::IMWRITE_JPEG_QUALITY;
-  param[1] = 80
+  param[1] = 80;
 
   crow::SimpleApp app; 
 
   CROW_WEBSOCKET_ROUTE(app, "/ws")
-    .on_open([&](crow::websocket::connection& conn) {
+    .onopen([&](crow::websocket::connection& conn) {
     })
-    .on_close([&](crow::websocket::connection& conn, const std::string& reason
+    .onclose([&](crow::websocket::connection& conn, const std::string& reason,
     uint16_t status_code) {
-      cap.release(); 
     })
-    .on_message([&](crow::websocket::connection& conn, const std::string& message,
+    .onmessage([&](crow::websocket::connection& conn, const std::string& message,
     bool is_binary) {
-      if (frame == NULL)
-        return; 
+      std::string result = "Error";
 
-      bool is_sucess = cv::imencode(".jpg", *frame, buff, param); 
+      if (frame == NULL)
+        return result; 
+      else if (frame->empty())
+        return result; 
+
+      bool is_success = cv::imencode(".jpg", *frame, buff, param); 
       
       if (!is_success)
         return result; 
 
-        for (uchar c: buff)
-          result.push_back(c); 
+      result.clear(); 
+      for (uchar c: buff)
+        result.push_back(c); 
       
-      return buff;
+      return result;
     })
 
   app.port(18080).multithreaded().run_async(); 
